8434988b9fb33ab00f52cb1664d1c140faec690b
g2mt <pm2mtr@gmail.com>
2026-05-12 19:18:12 +0000

    feat: add stubs for double ended implementation of entry iterator

--- src/database/iter.rs
+++ src/database/iter.rs
@@ -5,21 +5,33 @@ use anyhow::Result;
 use super::Entry;
 use crate::database::BUFFER_SIZE;
 
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Forward,
+    Backward,
+}
+
 pub struct Iter<'a, Backing: Seek + Read> {
     backing: BufReader<&'a mut Backing>,
-    is_first: bool,
+    head_offset: Option<u64>, // points to the next, unread element
+    tail_offset: Option<u64>, // points to the previous, unread element
+    seek_dir: Direction,
+    had_error: bool,
 }
 
 impl<'a, Backing: Seek + Read> Iter<'a, Backing> {
     pub(super) fn new(backing: &'a mut Backing) -> Self {
         Self {
             backing: BufReader::new(backing),
-            is_first: true,
+            head_offset: None,
+            tail_offset: None,
+            seek_dir: Direction::Forward,
+            had_error: false,
         }
     }
 
-    fn go_first(&mut self) -> Result<()> {
-        if !self.is_first {
+    fn calc_first_entry_offset(&mut self) -> Result<()> {
+        if self.head_offset.is_some() {
             return Ok(());
         }
         self.backing.seek(SeekFrom::Start(0))?;
@@ -37,7 +49,8 @@ impl<'a, Backing: Seek + Read> Iter<'a, Backing> {
                 break;
             }
         }
-        self.is_first = false;
+        let offset = self.backing.seek(SeekFrom::Current(0))?;
+        self.head_offset = Some(offset);
         Ok(())
     }
 }
@@ -46,8 +59,30 @@ impl<'a, Backing: Seek + Read> Iterator for Iter<'a, Backing> {
     type Item = Result<Entry>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Err(e) = self.go_first() {
-            return Some(Err(e));
+        macro_rules! iter_try {
+            ($expr:expr) => {{
+                match $expr {
+                    Ok(x) => x,
+                    Err(e) => {
+                        self.had_error = true;
+                        return Some(Err(e.into()));
+                    }
+                }
+            }};
+        }
+        if self.had_error {
+            return None;
+        }
+        match self.seek_dir {
+            Direction::Forward => {
+                iter_try!(self.calc_first_entry_offset());
+            }
+            Direction::Backward => {
+                self.seek_dir = Direction::Forward;
+                if let Some(head_offset) = self.head_offset {
+                    iter_try!(self.backing.seek(SeekFrom::Start(head_offset)));
+                }
+            }
         }
         let mut line = Vec::new();
         let n = self.backing.read_until(b'\n', &mut line).ok()?;
@@ -61,5 +96,11 @@ impl<'a, Backing: Seek + Read> Iterator for Iter<'a, Backing> {
             return None;
         }
         Some(serde_json::from_slice(&line).map_err(Into::into))
+    }
+}
+
+impl<'a, Backing: Seek + Read> DoubleEndedIterator for Iter<'a, Backing> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
